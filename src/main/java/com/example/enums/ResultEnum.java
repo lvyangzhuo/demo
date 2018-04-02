795cb4ac28ce9d868d7003c6542099d30dbf7af7
“lvyangzhuo <lvyangzhuo@163.com>
2018-04-02 12:05:21 +0800

    测试git提交

--- src/main/java/com/example/enums/ResultEnum.java
+++ src/main/java/com/example/enums/ResultEnum.java
@@ -11,7 +11,8 @@ import lombok.Data;
 public enum ResultEnum {
     OK(200, "ok"),
     NO_18(100, "no 18"),
-    NO_13(101, "no 13");
+    NO_13(101, "no 13"),
+    DEMO(102,"demo");
     private Integer code;
     private String msg;
 
